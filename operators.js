40e98a388082bd0c2e704b6e0a7922d049949323
hritik4651 <hritik4651@gmail.com>
2024-07-29 01:00:23 +0530

    Operators.js

--- operators.js
+++ operators.js
@@ -1,12 +1,107 @@
-// Operators.
+////////////////////////////////////////////////////// Operators.
+
 
 // 1. Arithmentic Operators.
 
+if (false) {
+     let a = 2;
+     let b = 4;
+     let c;
+     c = a + b;
+     console.log('Addition - ' + c);
 
-// 2. Assignment Operators.
+     c = b - a;
+     console.log('Subtraction - ' + c);
+
+     c = b / a;
+     console.log('Divied - ' + c);
+
+     c = a * b;
+     console.log('Multiplication - ' + c);
+
+     c = b % a;
+     console.log('Modulus (Remainder) - ' + c);
+
+     c = b ** a;
+     console.log('Exponentiation - ' + c);
+
+     c++;
+     console.log('Increment - ' + c);
+
+     c--;
+     console.log('Decrement - ' + c);
+}
+
+// 2. Comparison (Relational) Operators.
+
+if (false) {
+     let a = 2;
+     let b = 4;
+     let c = 'hello';
+     let d = '2';
+     let e = 2;
+     if (a == d) {
+          console.log('a and b is equal.');
+     }
+     if (a === e) {
+          console.log('a and b  values or datatypes are equal. ');
+     }
+     if (a != c) {
+          console.log('a and c is not equal.');
+     }
+     if (a !== d) {
+          console.log('a and d values or datatypes are not equal');
+     }
+     if (b > a) {
+          console.log('B is Greater.');
+     }
+     if (a < b) {
+          console.log('A is smaller');
+     }
+     if (a <= e) {
+          console.log('A is equal to E.');
+     }
+     if (e >= a) {
+          console.log('E is equal to A.');
+     }
+}
 
 
-// 3. Comparison Operators.
+
+// 3. Assignment Operators.
+
+if (false) {
+     let a = 2;
+     let b = 4;
+     let c;
+     c = 'Shivaay'; // = Basic Assignment Operator.
+     console.log(c);
+
+     a += b; // Addition assignment.
+     console.log(a);
+
+     a -= b; // Subtraction assignment. 
+     console.log(a);
+
+     a *= b; // Multiplication assignment. 
+     console.log(a);
+
+     a /= b; // Division assignment.
+     console.log(a);
+
+     b %= a; // Remainder assignment
+     console.log(b);
+
+     b = 8;
+     b **= a; // Exponentiation assignment.
+     console.log(b);
+}
 
 
-// 3. Bitwise Operators.
+// 4. Bitwise Operators.
+
+
+// 5. Logical Operators.
+
+
+// 6. Special Operators.
