64cc6133e4b6986645131dc9d1f969d4c6082396
hritik4651 <hritik4651@gmail.com>
2024-07-30 19:45:27 +0530

    if.js

---
+++ if.js
@@ -0,0 +1,16 @@
+// Conditional Statement
+
+// 1. if Statement.
+
+if (false) {
+     let x = 21;
+     if (x < 18) {
+          console.log('You are Underage. ');
+     }
+     if (x > 18) {
+          console.log('You are adult. ');
+     }
+}
+
+
+
---
+++ test.js
@@ -0,0 +1,12 @@
+console.time('test');
+
+console.log('hey');
+
+console.clear();
+
+console.error('err');
+
+console.warn('warn');
+console.table([1,2,3,4,5]);
+
+console.timeEnd('test');
