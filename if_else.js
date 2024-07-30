5bea3b97ec99de4ce54388189b44b3073259fbb8
hritik4651 <hritik4651@gmail.com>
2024-07-30 19:53:06 +0530

    if_else.js

---
+++ if_else.js
@@ -0,0 +1,12 @@
+// Conditional Statement
+
+// 1. if else Statement.
+
+if (true) {
+     let no = 10;
+     if (no % 2 == 0) {
+          console.log('Number is Even.');
+     } else {
+          console.log('Number is Odd.');
+     }
+}
