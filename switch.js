7773b125d3a064b83bb3eab6dc041e6acd1a3eb3
hritik4651 <hritik4651@gmail.com>
2024-07-30 20:13:40 +0530

    Switch.js

---
+++ switch.js
@@ -0,0 +1,33 @@
+// Conditional Statement
+
+// 4. Switch Statement.
+
+if (true) {
+     let day = 1;
+     switch (day) {
+          case 0:
+               console.log('Today is SunDay.');
+               break;
+          case 1:
+               console.log('Today is MonDay.');
+               break;
+          case 2:
+               console.log('Today is TuesDay.');
+               break;
+          case 3:
+               console.log('Today is WednesDay.');
+               break;
+          case 4:
+               console.log('Today is ThursDay.');
+               break;
+          case 5:
+               console.log('Today is FriDay.');
+               break;
+          case 6:
+               console.log('Today is SaturDay.');
+               break;
+
+          default:
+               break;
+     }
+}
